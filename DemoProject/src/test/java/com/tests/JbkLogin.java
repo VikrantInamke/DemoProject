1e69f840625f81c350ec3a6a8ad33cccf78c7249
hp <hp@LAPTOP-4RLFGVFV>
2024-04-30 23:00:20 +0530

    First Commit Of DemoProject

---
+++ DemoProject/src/test/java/com/tests/JbkLogin.java
@@ -0,0 +1,21 @@
+package com.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class JbkLogin {
+	
+	@Test
+	public void verifyLogin(){
+		System.setProperty("webdriver.chrome.driver", "D:\\JAVA\\WebDriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver() ;
+		driver.get("file:///D:/JAVA/SELENIUM/Selenium%20Softwares/Selenium%20Softwares/Offline%20Website/index.html");
+		driver.findElement(By.id("email")).sendKeys("kiran@gmail.com");
+		driver.findElement(By.id("password")).sendKeys("123456");
+		driver.findElement(By.xpath("//button")).click();
+		Assert.assertEquals(driver.getTitle(), "JavaByKiran | Dashboard");
+	}
+}
